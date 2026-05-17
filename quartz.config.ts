caeaaed6a08ddf95baa241cfcdbade46943efa71
Tanmay Garg <tanmaygarg122@icloud.com>
2026-05-17 13:30:44 +0800

    Change analytics provider from plausible to goatcounter

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "goatcounter",
+      websiteID: "tanmaygarg"
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
